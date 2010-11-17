2ed416f2ef4e10bd2953fa9f21d8f6c37606c71c
Brian Shaler <brian@shaler.name>
2010-11-17 09:12:13 -0700

    Reorganize & add comments on configuration

--- processing/php-data-stream/db.php
+++ processing/php-data-stream/db.php
@@ -1,28 +1,69 @@
 <?
+/**
+* PHP-CLI CSV->MySQL Data Streamer
+* Author: Brian Shaler :-3)
+* Company: Data Realization
+* Date: November 16, 2010
+* 
+* This script is intended to take massive CSV text files, and stream the data 
+* into a MySQL database. Instead of opening the entire file, it reads chunks 
+* of the file (see $maxbuffer) and inserts line by line into the database 
+* using batch inserts up to $minquery characters in length. If the amount of 
+* data in memory is less than $minbuffer, another chunk of data is read. This 
+* goes on until the entire file has been streamed into the database. The CLI 
+* script can periodically output the status, showing how long the script has 
+* been executing and the number of lines inserted into the DB.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
+
+// Skip a number of rows, usually 1 row, which is a header row.
+// Run "head file.csv" to check the first few lines.
+$skip = 1;
+// In characters, minimum amount of data in memory
+// **IMPORTANT**
+// ** Make sure this number is greater than the maximum number 
+// of lines in a row **
+$minbuffer = 1000;
+// In characters, maximum amount of data in memory
+$maxbuffer = 10000;
+// In characters, minimum number of characters in an INSERT query
+$minquery = 2000;
+// In seconds, how frequently to output status
+$status_interval = 10;
+// In seconds, set a maximum time for the script to run.
 set_time_limit(10000);
 
-$skip = 1;
-$skipped = 0;
-$minbuffer = 1000;
-$maxbuffer = 10000;
-$minquery = 2000;
-
+// File to read
 $file = "data.csv";
+// Table to insert into
 $table = "extract";
+
 $fields = array(  array("timestamp", "varchar"), 
                   array("double1", "double"),
                   array("double2", "double"),
                   array("int1", "int"),
                   array("char1", "varchar"));
 
+$dblink = mysql_connect("database_host", "database_user", "database_password");
+mysql_select_db("database_name");
+
+// CONFIGURATION VARIABLES ABOVE THIS LINE
+
+
+// Now on to the fun part :-3)
+
+$query = "";
+$skipped = 0;
 $rows_inserted = 0;
 $start_time = time();
 $last_time = $start_time;
-$checkup_time = 10;
-
-$dblink = mysql_connect("database_host", "database_user", "database_password");
-mysql_select_db("database_name");
-$query = "";
 
 function get_first_line ($str)
 {
@@ -64,7 +105,7 @@ function hms ($seconds)
 
 function process_line ($line)
 {
-  global $query, $dblink, $table, $fields, $minquery, $start_time, $last_time, $checkup_time, $rows_inserted;
+  global $query, $dblink, $table, $fields, $minquery, $start_time, $last_time, $status_interval, $rows_inserted;
   
   $record = explode(",", $line);
   if ($query == "")
@@ -111,7 +152,7 @@ function process_line ($line)
     $query = "";
   }
   
-  if ($last_time + $checkup_time <= time())
+  if ($last_time + $status_interval <= time())
   {
     echo "Run time: ".hms(time()-$start_time)."\n";
     echo "Rows: $rows_inserted\n";
